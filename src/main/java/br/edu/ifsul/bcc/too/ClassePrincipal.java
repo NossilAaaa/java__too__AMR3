c6f2ac70b6f16b73170f4d6a06b29afeb53966cd
20212PF.CC0020 <20212PF.CC0020@pc-155967.passofundo.ifsul.local>
2023-03-06 11:11:04 -0300

    TestedeAula

---
+++ src/main/java/br/edu/ifsul/bcc/too/ClassePrincipal.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifsul.bcc.too;
+
+/**
+ *
+ * @author 20212PF.CC0020
+ */
+public class ClassePrincipal {
+    
+}
---
+++ src/main/java/br/edu/ifsul/bcc/too/Java_maven_too_AMR3_23.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.ifsul.bcc.too;
+
+/**
+ *
+ * @author 20212PF.CC0020
+ */
+public class Java_maven_too_AMR3_23 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
